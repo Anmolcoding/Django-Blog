367b0522ff02823e0c59844a527d4fb05ed814ca
kartik srivastava <kartik.srivastava0510@gmail.com>
2024-04-10 01:35:56 +0530

    deploy

--- firstapp/firstapp/settings.py
+++ firstapp/firstapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-eadn-7oh65lan#^iqnt4$_25rq_xkqwuu^)e_*f&&21&p%88a8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
